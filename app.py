b11473e2a048058f6ee1af96dab88c7fb8955e30
KeN095 <kevinx235@hotmail.com>
2020-09-24 16:29:50 -0400

    Added Comments

--- app.py
+++ app.py
@@ -37,12 +37,14 @@ def index():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    #appointment creation page that passes in the days of the week as an array for the calendar table
     return render_template('create.html', days = ['Sun', 'Mon','Tue','Wed','Thu','Fri','Sat'] )
 
 @app.route('/success', methods=['GET', 'POST'])
 def success():
+    #This portion is reached from the form in the create page
         if request.method == 'POST':
-        
+            #Checks if the method of the request is POST and if it is, assign variables from the form and generate a random appointment ID
             firstName = request.form['fName']
             lastName = request.form['lName']
             insuranceID = request.form['insuranceID']
@@ -55,12 +57,15 @@ def success():
             #push request.form data into appointment object instead of storing them into variables
 
             try: 
-        
+                #In the try block, a new appointment object is created with all variables from the form 
                 new_apt = appointment(aptID, insuranceID, firstName, lastName, phone_number, email, doctor, extraInfo)
+
+                #Taking the data and entering it into the database then committing that action
 
                 db.session.add(new_apt)
                 db.session.commit()
                 
+                #Establishing session storing credentials in the session. Can be used to determine if redirects happen or not
                 session['patient'] = firstName
                 session['lastName'] = lastName
                 session['phoneNum'] = phone_number
@@ -77,39 +82,33 @@ def success():
                 #if there is an error, it is because the primary key, the insurance ID, is the same in the database
 
             except:
+                #If there is a failure in entering in appointment data then the message below will be displayed
                 return 'There was an issue adding your appointment to the system. Please make sure all your details are correct'   
         if "lastName" in session:
+            #Most likely will show the same success page long after appointment was created
             return render_template('success.html')
         else:
             #flash("You have not yet made an appointment.")
+            #If there is no session data then redirect to the home page to create an appointment
             return redirect(url_for('index'))
 
 @app.route("/delete", methods = ['GET','POST'])
 def delete():
-    '''
-    check if data is in session, if it is, redirect to delete page
-    if not, redirect to search page
-
-    search page should either return all appointments by name and insurance ID
-    or return one appointment based on appointment ID and ask if appoinment wants to be cancelled
-    '''
+    #This page can delete a users appointment
     if "lastName" in session:
+        #First checks if the user is in session and if they are, then enter in the try block
         try:
             #appointment_to_delete = appointment.query.filter_by(aptID = session["aptID"]).first()
             #above statement works for querying based on primary key
 
-            
+            #Finding the appointment ID by using the appointmentID in the session and then deleting that
             appointment.query.filter_by(aptID = session["aptID"]).delete()
             db.session.commit()
 
             doctor = session['doctor']
-
-            #db.session.delete(appointment_to_delete)
+            #setting doctor variable equal to the one in session to display it in the next page
             
-
-            #also test if data still exists after deleting the row by inserting it into delete html page
-
-            
+            #Once appointment is deleted then pop all data from session
             session.pop("lastName",None)
             session.pop("aptID",None)
             session.pop("doctor",None)
@@ -120,11 +119,14 @@ def delete():
             
             return render_template('delete.html', doctor = doctor)
         except:
+            #Below message is displayed if appointment was unable to be deleted
             return "Unable to delete data. Try again."
 
 @app.route("/update", methods = ['GET','POST'])
 def update():
     #try to utilize create page
+
+    #Incomplete but allows to update appointment information
 
     if "patient" in session:
         return render_template("update.html")
@@ -134,23 +136,35 @@ def update():
 
 @app.route("/search", methods = ['GET','POST'])
 def search():
+    #Allows to obtain appointment information by inputting appointment ID
     if request.method == 'POST':
+        #If the user inputs an appointment ID, then assign the aptID to the value that was entered in
         aptID = request.form['aptSearch']
       
         try:
+            #set info equal to the object obtained in database
             info = appointment.query.filter_by(aptID = aptID).first()
+
+            #Passing in info object, whether it is defined or not
             return render_template('search.html', info = info)
         
         except:
             return "Something went wrong.."
     
     else:
+        #Search page is displayed if visited normally
+
+        '''
+        On arrival of search page, there are checks made in jinja.
+        If the user is not in the session, then show the appointment ID field.
+        If they are in the session then display a table filled with the appointment details and display
+        2 additional buttons to either update their appointment details or cancel it
+
+        If the user is not in the session and inputs an appointment ID to search for and is valid, then display the details for the appointment
+        '''
         return render_template("search.html")
 
-#email confirmation
-#input checking
-#input patient data into database
-
 if __name__ == "__main__":
+    #create db file if none exists
     db.create_all()
     app.run(debug=True)
